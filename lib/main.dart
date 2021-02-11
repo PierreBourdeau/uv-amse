4b844b6779f559a036c43ebcdabf3cb18233c532
Askiipp <pierre.bourdeau1@outlook.fr>
2021-02-11 12:02:36 +0100

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+final series = [
+  MediaModel(
+    imageUrl:
+    'https://m.media-amazon.com/images/M/MV5BNzRlNGUzMmEtYTg0Ni00N2U2LTg4YWEtNDdlNmMwYjBlZDQ0XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UY864_.jpg',
+    title: 'Lupin',
+    description: "Série Netflix ...",
+  ),
+  MediaModel(
+    imageUrl:
+    'https://upload.wikimedia.org/wikipedia/fr/6/67/StrangerThingslogo.png',
+    title: 'Stranger Things',
+    description: "Série Netflix ...",
+  ),
+  MediaModel(
+    imageUrl:
+    'https://m.media-amazon.com/images/M/MV5BNGEyOGJiNWEtMTgwMi00ODU4LTlkMjItZWI4NjFmMzgxZGY2XkEyXkFqcGdeQXVyNjcyNjcyMzQ@._V1_FMjpg_UX675_.jpg',
+    title: 'The Boys',
+    description: "Série Amazon ...",
+  ),
+];
+
+final bds = [
+  MediaModel(
+    imageUrl: 'images/bds/aldebaran.jpg',
+    title: 'Aldébaran',
+    description: "Les mondes d'Aldébaran ...",
+  ),
+  MediaModel(
+    imageUrl: 'images/bds/le_tueur.jpg',
+    title: 'Le tueur',
+    description: "Bd cynique sur le monde...",
+  ),
+];
+void main() => runApp(MyApp());
+
+/// This is the main application widget.
+class MyApp extends StatelessWidget {
+  static const String _title = 'TP1 Mediateque';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+/// This is the stateful widget that the main application instantiates.
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static List<Widget> _widgetOptions = <Widget>[
+    ListView.builder(itemBuilder: (context, index) {
+      return Card(
+        child: ListTile(
+          onTap: (){},
+          title: Text(series[index].title),
+          trailing: Image.network(series[index].imageUrl),
+          subtitle: Text(series[index].description),
+        ),
+      );
+    }, itemCount: series.length, padding: const EdgeInsets.all(8),),
+    ListView.builder(itemBuilder: (context, index) {
+      return Card(
+        child: ListTile(
+          onTap: (){},
+          title: Text(bds[index].title),
+          trailing: Image.network(bds[index].imageUrl),
+          subtitle: Text(bds[index].description),
+        ),
+      );
+    }, itemCount: series.length, padding: const EdgeInsets.all(8),)
+  ];
+
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TP1 Mediateque'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.play_arrow),
+            label: 'Series',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_books),
+            label: 'BD',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.music_note),
+            label: 'Musique',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class MediaModel {
+  String imageUrl;
+  String title;
+  String description;
+
+  // Constructor
+  MediaModel({this.imageUrl, this.title, this.description});
+}
+
+
+
